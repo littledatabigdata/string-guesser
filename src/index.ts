045d9fa418b8ec4c68fe36130710ae1100c6f5f2
littledatabigdata <dhguo@connect.ust.hk>
2021-06-27 00:54:16 +0800

    refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,112 +1,19 @@
+import {qgram, cosine, jaccard, jaroWinker} from './comparer';
 
-function hammingDist(a: string, b: string): number {
-    if(typeof a != "string" || typeof b != "string") throw 'arguments are not strings'
 
-    let dist = 0;
-    const minLen = Math.min(a.length, b.length)
+export function compare(a: string, b: string) {
+    let jw = (q: number) => () => jaroWinker(a, b, q)
 
-    for (let i = 0; i < minLen; i++) {
-        if(a[i] === b[i]) dist++
-    }
+    let func = [cosine, jaccard, jw(0.1), jw(0.2)]
+    let res = func.map(fun => fun.apply(null, [a, b]))
+    console.log(func.map(x => x.name))
+    console.log(res)
 
-    return dist;
+    return res;
 }
 
-// edit distance
-function levenshtein(a: string, b: string): number {
-	let tmp;
-    if (a.length === 0) { return b.length; }
-	if (b.length === 0) { return a.length; }
-	if (a.length > b.length) { tmp = a; a = b; b = tmp; }
-
-	let i, j, dist, alen = a.length, blen = b.length, row = Array(alen);
-	for (i = 0; i <= alen; i++) { row[i] = i; }
-
-	for (i = 1; i <= blen; i++) {
-		dist = i;
-		for (j = 1; j <= alen; j++) {
-			tmp = row[j - 1];
-			row[j - 1] = dist;
-			dist = b[i - 1] === a[j - 1] ? tmp : Math.min(tmp + 1, Math.min(dist + 1, row[j] + 1));
-		}
-	}
-
-	return dist;
-}
-
-
-// n-gram vector strings between two strings
-function qgram(a: string, b: string, q = 2): Array<string> {
-    let vectors: Array<string> = [];
-
-    for(let i = 0; i < a.length - 1; i++) {
-        vectors.push(a.slice(i, i + q))
-    }
-
-    for(let j = 0; j < b.length - 1; j++) {
-        vectors.push(b.slice(j, j + q))
-    }
-
-    return Array.from(new Set(vectors))
-}
-
-
-// sum of absolute differences between
-// contiguous N gram vectors of two strings
-function qgramDist(a: string, b: string, q: number = 2): number {
-    if(typeof a != "string" || typeof b != "string") throw 'arguments are not strings'
-
-    let dist = 0;
-    let vectors = qgram(a, b, q)
-
-    vectors.forEach(vector => {
-        if(!a.includes(vector) || !b.includes(vector)) {
-            dist++;
-        }
-    })
-
-    return dist;
-}
-
-// cosine similarity
-function cosineDist(a: string, b: string, q: number = 2): number {
-    let vectors = qgram(a, b, q)
-    let aVec = vectors.map(x => a.includes(x) ? 1 : 0)
-    let bVec = vectors.map(x => b.includes(x) ? 1 : 0)
-
-    // norm
-    const dot = (v1: Array<number>, v2: Array<number>) => {
-        return v1.map((x, i) => v1[i] * v2[i]).reduce((m, n) => m + n);
-    }
-
-    // dot product
-    let numerator = dot(aVec, bVec)
-    let denom = Math.sqrt(dot(aVec, aVec)) * Math.sqrt(dot(bVec, bVec))
-
-    return 1 - numerator/denom;
-}
-
-function jaccard(a: string, b: string, q: number = 2): number {
-    let vectors = qgram(a, b, q)
-    let shared = vectors.filter(v => a.includes(v) && b.includes(v))
-
-    return 1 - shared.length / vectors.length
-}
-
-function jaroWinker(a: string, b: string, p: number = 0.1): number {
-    const dist = (al: number, bl: number, m: number, t: number) => {
-        return 1 - (1/3) * (m/al + m/bl + (m - t)/m)
-    }
-
-    let tmp;
-	if (a.length > b.length) { tmp = a; a = b; b = tmp; }
+export function compareList(a: string, b: string[]): string {
     
-    let t = levenshtein(a, b)
 
-
-
-    return 0;
+    return a;
 }
-
-
-console.log(jaccard('abcde','abdcde'))
